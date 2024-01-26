a0334fb2011de2cc565034a14a106731748e499a
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-26 21:33:42 +0600

    recap

---
+++ recap.js
@@ -0,0 +1,32 @@
+// Variable ==> container
+
+// number
+var penPrice = 10; 
+
+// string
+var subject = "CSC";
+
+// boolean
+var passed = true;
+
+// Naming Convention
+/*
+1. single word
+2. no quote
+3. no gap or dash
+4. can not start with number
+5. can not be a keyword
+6. prefer camelcase
+ */
+
+var camelCase = true;
+
+// Arithmetic Operation
+
+/*
+1. +, -, *, /, %
+2. +=, -+, *=, /=
+3. isNaN
+4. null
+5. Number
+*/
