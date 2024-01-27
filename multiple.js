dffbe5353ce4638f7762bea285503bf27c8cd5dc
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-27 09:34:14 +0600

    multiple added

---
+++ multiple.js
@@ -0,0 +1,10 @@
+const salary = 30000;
+const isBCS = true;
+const height = 61;
+const hasCar = false
+
+if ((salary>20000 && height>65)||(isBCS==true && hasCar==true) ) {
+  console.log('Welcome');
+} else {
+  console.log('Not Welcome');
+}
