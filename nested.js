6c9f3fab60cbebd42d290bcb537bb3a5fdfb23c9
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-27 10:05:05 +0600

    nested-if-else added

---
+++ nested.js
@@ -0,0 +1,14 @@
+const money = 101;
+if (money>300) {
+  console.log('bro is Rich'); 
+} else {
+  if (money>100) {
+    console.log('bro is not Rich neither Poor');
+  } else {
+    if (money>0) {
+      console.log('Eat banana');
+    } else {
+      console.log('404 money is not found');
+    }
+  }
+}
