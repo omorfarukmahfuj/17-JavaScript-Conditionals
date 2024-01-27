26bdfaf51dedfdb0641918125a4e1fe96be3363f
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-01-27 16:04:26 +0600

    ternary-number.js added

---
+++ Practice Task/ternary-number.js
@@ -0,0 +1,17 @@
+const num1 = 45;
+const num2 = 16;
+
+let result;
+
+/* 
+if (num1 > num2) {
+  result = num1 * 2;
+  console.log(result);
+} else {
+  result = num1 + num2;
+  console.log(result);
+}
+*/
+
+result = num1>num2 ? num1*2 : num1+num2;
+console.log(result);
